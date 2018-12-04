9fa0083c64506965d17c4269cddb0bdda26a3a97
hibachihoe <43859732+hibachihoe@users.noreply.github.com>
2018-12-04 10:05:31 -0500

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,1 @@
+print('gretchenannpfluger' * 1000)
---
+++ problem6.py
@@ -0,0 +1,5 @@
+firstname = 'Albus'
+lastname = 'Dumbledore'
+print('Firstname:' + " " + firstname)
+print('Lastname:' + " " + lastname)
+print('Fullname:' + " " + firstname + " " + lastname)
